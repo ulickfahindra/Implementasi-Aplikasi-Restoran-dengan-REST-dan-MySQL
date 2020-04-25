069a81fc5f0670b7d8c89dface87737d037e5e7e
ulickfahindra <56035369+ulickfahindra@users.noreply.github.com>
2020-04-25 19:45:23 +0700

    Add files via upload

---
+++ webservice/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+ define('HOST','localhost');
+ define('USER','id13373289_resto');
+ define('PASS','KkmsFy@yCXQNC2V!');
+ define('DB','id13373289_restoran');
+
+ $con = new mysqli(HOST,USER,PASS,DB) or die('Unable to Connect');
+ ?>
